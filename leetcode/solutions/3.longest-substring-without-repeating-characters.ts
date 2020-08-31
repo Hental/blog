e9c1a454d5ae6a69267cedcb39349d8aa4161db6
tao.liu <lt315417284@gmail.com>
2020-08-31 22:11:51 +0800

    leetcode: 3, 136

--- leetcode/solutions/3.longest-substring-without-repeating-characters.ts
+++ leetcode/solutions/3.longest-substring-without-repeating-characters.ts
@@ -7,22 +7,34 @@
 // @lc code=start
 function lengthOfLongestSubstring(s: string): number {
   const map = new Map<string, number>();
-  let maxSize = 0;
+
+  let maxSize = -1;
   let start = 0;
+
   for (let i = 0; i < s.length; i += 1) {
     let char = s[i];
-    if (map.has(char)) {
-      maxSize = Math.max(maxSize, map.size);
+    let charPrevIndex = map.get(char);
+
+    if (charPrevIndex !== undefined && charPrevIndex >= start) {
+      maxSize = Math.max(maxSize, i - start);
       let idx = map.get(char) as number;
-      for (let j = start; j <= idx; j += 1) {
-        map.delete(s[j]);
-      }
       start = idx + 1;
     }
+
     map.set(char, i);
   }
-  maxSize = Math.max(maxSize, map.size);
-  return maxSize;
+
+  return Math.max(maxSize, s.length - start);
 };
 // @lc code=end
 
+describe('3.3.longest-substring-without-repeating-characters', () => {
+  it('solution', () => {
+    expect(lengthOfLongestSubstring('abba')).toBe(2);
+    expect(lengthOfLongestSubstring('aab')).toBe(2);
+    expect(lengthOfLongestSubstring('pwwkew')).toBe(3);
+    expect(lengthOfLongestSubstring('abcabcbb')).toBe(3);
+    expect(lengthOfLongestSubstring('bbbbb')).toBe(1);
+    expect(lengthOfLongestSubstring(' ')).toBe(1);
+  });
+});
