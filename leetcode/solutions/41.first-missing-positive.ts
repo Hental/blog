fa761d0cdb2b4b2b34302f79c220795103bef610
tao.liu <lt315417284@gmail.com>
2020-09-04 00:13:01 +0800

    leetcode: 41

--- leetcode/solutions/41.first-missing-positive.ts
+++ leetcode/solutions/41.first-missing-positive.ts
@@ -6,29 +6,33 @@
 
 // @lc code=start
 function firstMissingPositive(nums: number[]): number {
-  const max = nums.length + 1;
-  const min = 1;
-  const set = new Set<number>();
-
   for (let index = 0; index < nums.length; index++) {
-    const num = nums[index];
-    set.add(num);
-  }
-
-  for (let j = min; j < max; j++) {
-    if (!set.has(j)) {
-      return j
+    if (nums[index] <= 0) {
+      nums[index] = nums.length + 1;
     }
   }
 
-  return max;
+  for (let index = 0; index < nums.length; index++) {
+    let val = Math.abs(nums[index]);
+    if (val <= nums.length) {
+      nums[val - 1] = -Math.abs(nums[val - 1]);
+    }
+  }
+
+  for (let index = 0; index < nums.length; index++) {
+    if (nums[index] > 0) {
+      return index + 1;
+    }
+  }
+
+  return nums.length + 1;
 };
 // @lc code=end
 
 describe('41.First Missing Positive', () => {
   it('solution', () => {
-    expect(firstMissingPositive([-1,4,2,1,9,10])).toBe(3);
     expect(firstMissingPositive([1, 2, 0])).toBe(3);
+    expect(firstMissingPositive([-1,4,2,1,9,10])).toBe(3);
     expect(firstMissingPositive([3, 4, -1, 1])).toBe(2);
     expect(firstMissingPositive([7,8,9,11,12])).toBe(1);
   });
